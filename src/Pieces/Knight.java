fee85e2d6e1bf70a0378e777f6d72971183f3a01
matthes <matthesboettcher03@gmail.com>
2024-09-05 21:46:37 +0200

    Klasse fuer Ritter aktualisiert

--- src/Pieces/Knight.java
+++ src/Pieces/Knight.java
@@ -1,13 +1,32 @@
 package Pieces;
 
+import Logic.CalculateHorizontals;
+import Logic.CalculateKnightJumps;
+
+import java.util.ArrayList;
+
+import static GameData.Data.setColorPGN;
+
 public class Knight extends Piece {
     private final String color;
+    private int[] position;
+    private ArrayList<int[]> potentialMoves = new ArrayList<>();
 
     public Knight(String color) {
         this.color = color;
     }
 
     public String getName() {
-        return "N"+color;
+        return "R"+color;
+    }
+
+    public void calculateMoves(int y, int x) {
+        position = new int[]{y,x};
+
+        CalculateKnightJumps ck = new CalculateKnightJumps();
+        ck.setPosition(position);
+        potentialMoves = ck.calculateMoves();
+
+        setColorPGN(potentialMoves);
     }
 }
