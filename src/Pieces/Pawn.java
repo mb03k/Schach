76123aa22bf23f242c32bb22b0ca2a794c56687e
matthes <matthesboettcher03@gmail.com>
2024-09-05 19:39:55 +0200

    potentielle Zuege markieren

--- src/Pieces/Pawn.java
+++ src/Pieces/Pawn.java
@@ -1,7 +1,16 @@
 package Pieces;
 
+import Logic.CalculateHorizontals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static GameData.Data.setColorPGN;
+
 public class Pawn extends Piece {
+    private int[] position;
     private final String color;
+    private ArrayList<int[]> potentialMoves = new ArrayList<>();
 
     public Pawn(String color) {
         this.color = color;
@@ -9,5 +18,19 @@ public class Pawn extends Piece {
 
     public String getName() {
         return "P"+color;
+    }
+
+    public void calculateMoves(int y, int x) {
+        position = new int[]{y, x};
+
+        CalculateHorizontals ch = new CalculateHorizontals();
+        ch.setPosition(position);
+        potentialMoves = ch.calculateHorizontals();
+        System.out.println("Möglichkeiten:");
+        for (int[] potentialMove : potentialMoves) {
+            System.out.print(Arrays.toString(potentialMove));
+        }
+        System.out.println(" -> "+potentialMoves.size());
+        setColorPGN(potentialMoves);
     }
 }
