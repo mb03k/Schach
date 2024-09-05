2783c1673992fda0b4774b1e43ddbeca36ba3a9d
matthes <matthesboettcher03@gmail.com>
2024-09-05 21:47:14 +0200

    Klasse fuer Tuerme

--- src/Pieces/Rook.java
+++ src/Pieces/Rook.java
@@ -1,7 +1,15 @@
 package Pieces;
 
+import Logic.CalculateHorizontals;
+
+import java.util.ArrayList;
+
+import static GameData.Data.setColorPGN;
+
 public class Rook extends Piece {
     private final String color;
+    private int[] position;
+    private ArrayList<int[]> potentialMoves = new ArrayList<>();
 
     public Rook(String color) {
         this.color = color;
@@ -9,5 +17,15 @@ public class Rook extends Piece {
 
     public String getName() {
         return "R"+color;
+    }
+
+    public void calculateMoves(int y, int x) {
+        position = new int[]{y,x};
+
+        CalculateHorizontals ch = new CalculateHorizontals();
+        ch.setPosition(position);
+        potentialMoves = ch.calculateHorizontals();
+
+        setColorPGN(potentialMoves);
     }
 }
