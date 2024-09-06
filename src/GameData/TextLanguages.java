c51b684ef45d73f5ff48d4c4ba707e0e08d409b2
matthes <matthesboettcher03@gmail.com>
2024-09-06 10:41:47 +0200

    Dateien fuer naechste Implementierungen einbauen

---
+++ src/GameData/TextLanguages.java
@@ -0,0 +1,11 @@
+package GameData;
+
+public class TextLanguages {
+    public static final String[] homescreen_de = new String[] {
+        "Spielen", "Position erstellen", "Verlassen"
+    };
+
+    public static final String[] homescreen_en = new String[] {
+        "Play", "Set up position", "Quit"
+    };
+}
