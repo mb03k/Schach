ad2b2441d834350670777d6a1cf2165e8bbb7c8a
matthes <matthesboettcher03@gmail.com>
2024-09-07 20:41:33 +0200

    Logik fuer Schlagen implementiert

--- src/Logic/Logic.java
+++ src/Logic/Logic.java
@@ -1,6 +1,17 @@
 package Logic;
 
+import static GameData.Data.pgn;
+import static Gui.Play.currentPlayer;
+
 public abstract class Logic {
     protected final int y = 0;
     protected final int x = 1;
+
+    protected static boolean whitesMove() {
+        return currentPlayer.equals("w");
+    }
+
+    protected static boolean newPieceIsSameColor(int[] tempPosition, int y, int x) {
+        return pgn[tempPosition[y]][tempPosition[x]].getColor().equals(currentPlayer);
+    }
 }
