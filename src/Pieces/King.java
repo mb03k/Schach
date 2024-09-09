363901f5c07ee99d39b01447666f12d4719113a7
matthes <matthesboettcher03@gmail.com>
2024-09-09 11:10:15 +0200

    Logik fuer Koenig-Zuege

--- src/Pieces/King.java
+++ src/Pieces/King.java
@@ -1,11 +1,16 @@
 package Pieces;
 
+import Logic.CalculateDiagonals;
+import Logic.KingMoves;
+
 import java.util.ArrayList;
 
+import static GameData.Data.setColorPGN;
 import static Logic.CheckRequirements.takePiece;
 
 public class King extends Piece {
     private final String color;
+    private int[] position;
     private int[] newPosition;
     private ArrayList<int[]> potentialMoves;
 
@@ -23,6 +28,16 @@ public class King extends Piece {
         this.newPosition = new int[] {newy, newx};
 
         return takePiece(newPosition, potentialMoves);
+    }
+
+    public void calculateAndMarkMoves(int y, int x) {
+        position = new int[]{y, x};
+
+        KingMoves km = new KingMoves();
+        km.setPosition(position);
+        potentialMoves = km.calculateMoves();
+
+        setColorPGN(potentialMoves);
     }
 
 }
