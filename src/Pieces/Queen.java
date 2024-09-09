ee4c550d6c542102a2db92038a401a685a2974cd
matthes <matthesboettcher03@gmail.com>
2024-09-09 11:08:56 +0200

    Logik fuer Damenzuege

--- src/Pieces/Queen.java
+++ src/Pieces/Queen.java
@@ -1,11 +1,17 @@
 package Pieces;
 
-import java.util.ArrayList;
+import Logic.CalculateDiagonals;
+import Logic.CalculateHorizontals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static GameData.Data.setColorPGN;
 import static Logic.CheckRequirements.takePiece;
 
 public class Queen extends Piece {
     private final String color;
+    private int[] position;
     private int[] newPosition;
     private ArrayList<int[]> potentialMoves;
 
@@ -23,5 +29,26 @@ public class Queen extends Piece {
         this.newPosition = new int[] {newy, newx};
 
         return takePiece(newPosition, potentialMoves);
+    }
+
+    public void calculateAndMarkMoves(int y, int x) {
+        position = new int[]{y,x};
+        potentialMoves = new ArrayList<>();
+        ArrayList<int[]> potentialDiagonals;
+        ArrayList<int[]> potentialHorizontals;
+
+        CalculateDiagonals cd = new CalculateDiagonals();
+        CalculateHorizontals ch = new CalculateHorizontals();
+
+        cd.setPosition(position);
+        ch.setPosition(position);
+
+        potentialDiagonals = cd.calculateDiagonal();
+        potentialHorizontals = ch.calculateHorizontals();
+
+        potentialMoves.addAll(potentialDiagonals);
+        potentialMoves.addAll(potentialHorizontals);
+
+        setColorPGN(potentialMoves);
     }
 }
