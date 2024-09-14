52bd6b1b6e45a8fd331dd5d56231aed187a95581
matthes <matthesboettcher03@gmail.com>
2024-09-14 12:13:41 +0200

    Logik fuer Abfragen (ob Spieler dran ist) eingefuegt

--- src/Logic/CheckRequirements.java
+++ src/Logic/CheckRequirements.java
@@ -4,40 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import static GameData.Data.pgn;
-import static Gui.Play.currentPlayer;
 
 public class CheckRequirements {
     // only calculate moves if its the right piece color
     public static void checkSequenceAndCalculateMoves(int y, int x) {
-
-            pgn[y][x].calculateAndMarkMoves(y, x);
-        //checkIfPieceTake(y, x);
-
-        // whites move; white piece
-        /*if (pgn[y][x].getColor().equals("w") && currentPlayer.equals("w")) {
-
-        // blacks move; blacks piece
-        } else if (pgn[y][x].getColor().equals("b") && currentPlayer.equals("b")) {
-            pgn[y][x].calculateAndMarkMoves(y, x);
-        }*/
+        pgn[y][x].calculateAndMarkMoves(y, x);
     }
-
-    public static void checkIfPieceTake(int y, int x) {
-        System.out.print("checkIfPieceTake");
-        int[] position = new int[]{y, x};
-        ArrayList<int[]> potentialTakes = pgn[y][x].getPotentialTakes();
-        System.out.println(" größe: "+potentialTakes.size());
-
-        try {
-            for (int[] potentialTake : potentialTakes) {
-                System.out.println(Arrays.equals(position, potentialTake) + " -");
-                System.out.println("potentialTake: " + potentialTake[0] + "-" + potentialTake[1]);
-                System.out.println("position (wird übergeben): " + y + "-" + x);
-            }
-        } catch (NullPointerException ignored) {}
-        System.out.println();
-    }
-
 
     // can be simplified to one method
     public static boolean checkPieceMoves(int[] newPosition, ArrayList<int[]> potentialMoves) {
@@ -48,20 +20,28 @@ public class CheckRequirements {
         }
         return false;
     }
-    public static boolean checkPieceTake(int[] newPosition, int[] potentialPosition) {
-        for (int[] move : pgn[potentialPosition[0]][potentialPosition[1]].getPotentialTakes()) {
+
+    public static boolean checkPieceMoves(int[] newPosition, int[] potentialMoves) {
+        for (int[] move : pgn[potentialMoves[0]][potentialMoves[1]].getMoves()) {
             if (Arrays.equals(move, newPosition)) {
-                System.out.println("Figur auf "+newPosition[0]+"-"+newPosition[1]+" von "+move[0]+"-"+move[1]+" schlagbar");
                 return true;
             }
         }
         return false;
     }
 
-    public static boolean checkPieceTake(int[] newPosition, ArrayList<int[]> potentialPosition) {
-        for (int[] move : potentialPosition) {
+    public static boolean checkPieceTake(int[] newPosition, int[] oldPosition) {
+        for (int[] move : pgn[oldPosition[0]][oldPosition[1]].getPotentialTakes()) {
             if (Arrays.equals(move, newPosition)) {
-                System.out.println("Figur auf "+newPosition[0]+"-"+newPosition[1]+" von "+move[0]+"-"+move[1]+" schlagbar");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkPieceTake(int[] newPosition, ArrayList<int[]> oldPosition) {
+        for (int[] move : oldPosition) {
+            if (Arrays.equals(move, newPosition)) {
                 return true;
             }
         }
