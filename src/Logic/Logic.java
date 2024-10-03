f7b641b99c6e2ee03be742fd48a42b42c36056fd
Matthes Böttcher <matthesboettcher03@gmail.com>
2024-10-03 11:05:55 +0200

    Methode hinzugefuegt und andere angepasst um einen Bug zu praeventieren

--- src/Logic/Logic.java
+++ src/Logic/Logic.java
@@ -1,8 +1,9 @@
 package Logic;
 
+import static GameData.Data.blacksPM_PGN;
 import static GameData.Data.pgn;
+import static GameData.Data.whitesPM_PGN;
 import static Gui.Play.currentPlayer;
-
 import Pieces.EmptyField;
 
 public abstract class Logic {
@@ -13,16 +14,31 @@ public abstract class Logic {
         return currentPlayer.equals("w");
     }
 
-    protected static boolean newPieceIsSameColor(int[] position) {
-        return pgn[position[y]][position[x]].getColor().equals(currentPlayer);
+    /*
+     * Bug-fix:
+     * in the old version only the color of 'tempPosition' was compared with the string 'currentPlayer'
+     * but at the beginning of a game (stand of state: 03. October 24) the game starts with currentPlayer white
+     * so it returned with black every time false
+     */
+    protected static boolean newPieceIsSameColor(int[] oldPosition, int[] newPosition) {
+        return pgn[oldPosition[0]][oldPosition[1]].getColor().equals(pgn[newPosition[0]][newPosition[1]].getColor());
     }
 
     // white takes black or black takes white
-    protected static boolean pieceCanBeTaken(int[] tempPosition) {
-        return whitesMove() && !newPieceIsSameColor(tempPosition) || !whitesMove() && !newPieceIsSameColor(tempPosition);
+    protected static boolean pieceCanBeTaken(int[] position, int[] newPosition) {
+        //return whitesMove() && !newPieceIsSameColor(tempPosition) || !whitesMove() && !newPieceIsSameColor(tempPosition);
+        return !newPieceIsSameColor(position, newPosition);
     }
 
     protected static boolean isEmptyField(int y, int x) {
         return pgn[y][x] instanceof EmptyField;
+    }
+
+    protected static boolean otherColorSeesTheField(int y, int x) {
+        if (whitesMove()) {
+            return blacksPM_PGN[y][x] == 1;
+        }
+
+        return whitesPM_PGN[y][x] == 1;
     }
 }
