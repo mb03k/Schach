86ed412d3cba632a7103bce803cb054aa8fae531
Matthes Böttcher <matthesboettcher03@gmail.com>
2024-10-08 11:07:00 +0200

    Klasse etwas aufgeraeumt; Erstellung des Spielfeldes in extra Klasse verpackt

--- src/Gui/Play.java
+++ src/Gui/Play.java
@@ -7,9 +7,9 @@ import java.awt.*;
 import java.util.ArrayList;
 
 import static GameData.Data.*;
-import static java.lang.String.valueOf;
 import static java.lang.System.exit;
 import static Logic.Logic.*;
+import static Gui.PlayingField.*;
 
 public class Play {
 
@@ -28,7 +28,8 @@ public class Play {
 
     public void setPlayingField() {
         setMenuBar();
-        setPlayingFieldGridLayout();
+        fieldButtonPanel = new JPanel();
+        setPlayingFieldGridLayout(this.frame, fieldButtonPanel);
         board = new JPanel[8][8];
         setStandardPM_PGN();
 
@@ -72,6 +73,7 @@ public class Play {
 
     public void addPlayingFieldContent(int y, int x) {
         JPanel btnPanel = new JPanel();
+        //fieldButtonPanel = new JPanel();
         JButton playingFieldButtonListener;
         String nameOfPiece = pgn[y][x].getUi();
         JLabel emojiLabel = new JLabel(nameOfPiece);
@@ -129,79 +131,6 @@ public class Play {
                 changeSquareColor(i,j);
             }
         }
-    }
-
-    public void setPlayingFieldGridLayout() {
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.insets = new Insets(0, 0, 10, 0);
-
-        JPanel letterPanel = new JPanel(new GridLayout(1, 8));
-        letterPanel.setBackground(Color.DARK_GRAY);
-        gbc.gridwidth = 1;
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        gbc.weightx = 0.8;
-        gbc.weighty = 0.05;
-        frame.add(letterPanel, gbc);
-
-        JPanel numberPanel = new JPanel(new GridLayout(8, 1));
-        numberPanel.setBackground(Color.DARK_GRAY);
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.weightx = 0.1;
-        gbc.weighty = 0.8;
-        frame.add(numberPanel, gbc);
-
-        setPlayingFieldMarkers(letterPanel, numberPanel);
-        setPlayingFieldEdges(gbc);
-    }
-
-    public void setPlayingFieldMarkers(JPanel letterPanel, JPanel numberPanel) {
-        // Panel for labels (A-H)
-        for (int iterator = 0; iterator < 8; iterator++) {
-            JLabel letterLabel = new JLabel(valueOf((char) ('a' + iterator)), SwingConstants.CENTER);
-            letterLabel.setForeground(Color.WHITE);
-            letterLabel.setFont(new Font("Arial", Font.BOLD, 20));
-            letterPanel.add(letterLabel);
-        }
-        // Panel for labels (1-8)
-        for (int num=8; num>0; num--) {
-            JLabel numberLabel = new JLabel(valueOf(num), SwingConstants.CENTER);
-            numberLabel.setForeground(Color.WHITE);
-            numberLabel.setFont(new Font("Arial", Font.BOLD, 20));
-            numberLabel.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0)); // Abstand Rand
-            numberPanel.add(numberLabel);
-        }
-    }
-
-    public void setPlayingFieldEdges(GridBagConstraints gbc) {
-        // Spielfeld
-        fieldButtonPanel = new JPanel();
-        fieldButtonPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        fieldButtonPanel.setLayout(new GridLayout(8, 8));
-        fieldButtonPanel.setBackground(Color.DARK_GRAY);
-        gbc.gridx = 1;
-        gbc.gridy = 2;
-        gbc.weightx = 1;
-        gbc.weighty = 1;
-        frame.add(fieldButtonPanel, gbc);
-
-        // create and add edges
-        JPanel leftEdge = new JPanel();
-        JPanel rightEdge = new JPanel();
-        leftEdge.setBackground(Color.DARK_GRAY);
-        rightEdge.setBackground(Color.DARK_GRAY);
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.weightx = 0.1;
-        gbc.weighty = 0.8;
-        frame.add(leftEdge, gbc);
-        gbc.gridx = 2;
-        gbc.gridy = 2;
-        gbc.weightx = 0.1;
-        gbc.weighty = 0.8;
-        frame.add(rightEdge, gbc);
     }
 
     public void setMenuBar() {
@@ -297,22 +226,8 @@ public class Play {
         setPlayingField();
     }
 
-    public static void setCurrentPlayer(String cp) {
-        currentPlayer = cp;
-    }
-
-    public static String getCurrentPlayer() {
-        return currentPlayer;
-    }
-
     public void setHomescreen() {
-        frame.getContentPane().removeAll();
-        frame.setJMenuBar(null);
-        frame.repaint();
 
-        Homescreen sb = new Homescreen();
-        sb.setFrame(frame);
-        sb.setHomescreen();
     }
 
     public void saveGame() {
